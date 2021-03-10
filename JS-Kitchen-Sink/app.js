265eda3a8151ba22c8c98d73e071080d65e6129d
ethanjd23 <ethanjd23@gmail.com>
2021-03-10 15:50:56 -0600

    kitchen sink converted

--- JS-Kitchen-Sink/app.js
+++ JS-Kitchen-Sink/app.js
@@ -1,43 +1,51 @@
-let myName = "Ethan"; //my name
-const numberOfStates = 50; //number of states doesn't change
-let nine = 5 + 4; //addition
-function sayHello () {
+var myName = "Ethan"; //my name
+var numberOfStates = 50; //number of states doesn't change
+var nine = 5 + 4; //addition
+function sayHello() {
     alert("Hello World!");
-};
-sayHello()
-function checkAge (name, age) {
+}
+sayHello();
+function checkAge(name, age) {
     if (age < 21) {
-        alert("Sorry " + name + ", you aren't old enough to view this page!")};
-}; // Displays alert if person is not over 21
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+} // Displays alert if person is not over 21
 checkAge("Charles", 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
-checkAge('John', 17);
-let myFavoriteVegetables = ["Carrots", 'Beets', 'Potatoes', 'Green Peas', 'Sweet Potato'];
-for(let i = 0; i < myFavoriteVegetables.length; i++) {
+checkAge("John", 17);
+var myFavoriteVegetables = [
+    "Carrots",
+    "Beets",
+    "Potatoes",
+    "Green Peas",
+    "Sweet Potato",
+];
+for (var i = 0; i < myFavoriteVegetables.length; i++) {
     console.log(myFavoriteVegetables[i]);
-}; // loop to display all vegetables in the array
-let pet = {
+} // loop to display all vegetables in the array
+var pet = {
     name: "Rosie",
     breed: "Miniature Pinscher"
 };
 console.log(pet);
-let friends = [
+var friends = [
     { name: "Abbie", age: 22 },
     { name: "Emily", age: 19 },
     { name: "Jared", age: 13 },
     { name: "Lindsey", age: 22 },
     { name: "Mordecai", age: 22 },
-]
-for(let i = 0; i < friends.length; i++) {
-    checkAge(friends[i].name, friends[i].age)
-}; // checks age of objects in the friends array
-function getLength (word) {
-    let stringlength = word.length
-    if(stringlength % 2 == 0) {
-        console.log("The world is nice and even!")
-    } else {
-        console.log("The world is an odd place!")
+];
+for (var i = 0; i < friends.length; i++) {
+    checkAge(friends[i].name, friends[i].age);
+} // checks age of objects in the friends array
+function getLength(word) {
+    var stringlength = word.length;
+    if (stringlength % 2 == 0) {
+        console.log("The world is nice and even!");
     }
-}; // gets string's character count and changes msg depending on if it's odd or even
+    else {
+        console.log("The world is an odd place!");
+    }
+} // gets string's character count and changes msg depending on if it's odd or even
 getLength("Hello World");
---
+++ JS-Kitchen-Sink/app.ts
@@ -0,0 +1,50 @@
+let myName: string = "Ethan"; //my name
+const numberOfStates: number = 50; //number of states doesn't change
+let nine: number = 5 + 4; //addition
+function sayHello() {
+  alert("Hello World!");
+}
+sayHello();
+function checkAge(name: string, age: number) {
+  if (age < 21) {
+    alert("Sorry " + name + ", you aren't old enough to view this page!");
+  }
+} // Displays alert if person is not over 21
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+let myFavoriteVegetables: Array<string> = [
+  "Carrots",
+  "Beets",
+  "Potatoes",
+  "Green Peas",
+  "Sweet Potato",
+];
+for (let i = 0; i < myFavoriteVegetables.length; i++) {
+  console.log(myFavoriteVegetables[i]);
+} // loop to display all vegetables in the array
+let pet = {
+  name: "Rosie",
+  breed: "Miniature Pinscher",
+};
+console.log(pet);
+let friends: {name: string, age: number}[] = [
+  { name: "Abbie", age: 22 },
+  { name: "Emily", age: 19 },
+  { name: "Jared", age: 13 },
+  { name: "Lindsey", age: 22 },
+  { name: "Mordecai", age: 22 },
+];
+for (let i = 0; i < friends.length; i++) {
+  checkAge(friends[i].name, friends[i].age);
+} // checks age of objects in the friends array
+function getLength(word: string) {
+  let stringlength = word.length;
+  if (stringlength % 2 == 0) {
+    console.log("The world is nice and even!");
+  } else {
+    console.log("The world is an odd place!");
+  }
+} // gets string's character count and changes msg depending on if it's odd or even
+getLength("Hello World");
